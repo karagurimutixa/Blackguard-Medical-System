a21b78dcecd82045c1456e89579380cc68f0fb8e
Angus <cinar.yalcinkaya@gmail.com>
2024-12-08 11:05:51 +0300

    Update TrainerPage.js

--- src/components/TrainerPage.js
+++ src/components/TrainerPage.js
@@ -1,1 +1,21 @@
+import React from 'react';
 
+const TrainerPage = () => {
+  return (
+    <div>
+      <h1>Trainer Dashboard</h1>
+      <h2>Training Sessions</h2>
+      {/* Display a list of training sessions */}
+      <ul>
+        {/* Example training session item */}
+        <li>Training Session Title - <button>Edit</button> <button>Delete</button></li>
+      </ul>
+      <button>Create Training Session</button>
+      <h2>Trainee Progress</h2>
+      {/* Display progress reports for trainees */}
+      <p>Trainee progress will be displayed here.</p>
+    </div>
+  );
+};
+
+export default TrainerPage;
